70d7791892553e1277cbacc1884176dcb225ca37
Administrator <Administrator@ebaek-PC>
2014-08-16 18:09:52 +0900

    ordering

--- KRTT_WAS/src/main/java/com/passionpeople/krtt_was/dao/CompanyDao.java
+++ KRTT_WAS/src/main/java/com/passionpeople/krtt_was/dao/CompanyDao.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Order;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.passionpeople.krtt_was.vo.Company;
+import com.passionpeople.krtt_was.vo.UserAuth;
 
 @Repository
 public class CompanyDao  {
@@ -22,7 +26,10 @@ public class CompanyDao  {
     }
  
     public List<Company> getCompanyList() {
-        return mongoTemplate.findAll(Company.class, COLLECTION_NAME);
+    	Query query = new Query();
+    	query.addCriteria(new Criteria());
+    	query.sort().on("cpNm", Order.ASCENDING);
+        return mongoTemplate.find(query, Company.class, COLLECTION_NAME);
     }
     
     
